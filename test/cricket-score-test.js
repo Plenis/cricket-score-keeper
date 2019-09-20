a7ba4412f6e72eadfb16274b1fffc08b4f62df1c
Plenis <Plenis>
2019-09-20 10:39:00 +0200

    deploying to travis

---
+++ test/cricket-score-test.js
@@ -0,0 +1,10 @@
+let assert = require("assert");
+let cricketScoreKeeper = require("../cricket-score-keeper")
+
+describe('Cricket Score Keeper' , function(){
+    it('should get the total score for over that is added' , function(){
+        let instance = cricketScoreKeeper();
+        assert.equal();
+    })
+
+});

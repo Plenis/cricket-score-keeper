3dd6f45e011339b83435ce4352e784db7af86500
Plenis <Plenis>
2019-09-20 14:26:04 +0200

    factory functions + tests

--- cricket-score-keeper.js
+++ cricket-score-keeper.js
@@ -1,3 +1,89 @@
-function CricketScoreKeeper(){
-    
+function CricketScoreKeeper() {
+  let oversLeft = 0;
+  let oversPlayed = 0;
+  let overs = 0;
+  let currentScore = 0; 
+  let overCounter = 0;
+  let score = 0;
+  let wickets = 10;
+
+
+  function isValidScore(over) {
+    let overScore = Number(over);
+    let regex = /^[w]{2}\s\d[-0-9\s]{1,6}$/;
+    let result = regex.test(overScore);
+
+    for (let index = 0; index < result.length; index++) {
+      const validScore = result[index];
+      if (validScore === true) {
+      }
+    }
+    // return false;
+  }
+
+  function scoreKeeper(overScore) {
+    if (overs !== oversPlayed) {
+        const over = overScore || '';
+       
+        for (let i = 0; i < over.length; i++) {
+ switch (over[i]) {
+      case "w":
+        score += 0;
+      case "-":
+       score += 0;
+      case "1":
+        score += 1;
+      case "2":
+        score += 2;
+      case "3":
+        score += 3;
+      case "4":
+        score += 4;
+     case "5":
+        score += 5;
+    case "6":
+        score += 6;
+      default:
+        score += 0;
+    }
+  }
 }
+ currentScore += score;
+  }
+
+  function overScoreTotal(){
+      currentScore += score
+  }
+
+  function overallTotalScore(){
+    return overScoreTotal++
+  }
+
+  function setOver(overs) {
+    overs = oversLeft;
+    overCounter = overs * 6;
+    for (let i = 0; i < overs.length; i++) {
+      const element = overs[i];
+      element = overs;
+    }
+  }
+
+  function wicketsLeft(){
+      const wicketCheck = "w"
+     for (let index = 0; index < wicketCheck.length; index++) {
+         const element = wicketCheck[index];
+         wickets.element--
+     }
+  }
+
+  return {
+    isValidScore,
+    scoreKeeper,
+    overScoreTotal,
+    overallTotalScore,
+    setOver,
+    wicketsLeft
+  };
+}
+
+module.exports = CricketScoreKeeper;
--- test/cricket-score-test.js
+++ test/cricket-score-test.js
@@ -1,10 +1,56 @@
 let assert = require("assert");
-let cricketScoreKeeper = require("../cricket-score-keeper")
+let CricketScoreKeeper = require("../cricket-score-keeper");
 
 describe('Cricket Score Keeper' , function(){
-    it('should get the total score for over that is added' , function(){
-        let instance = cricketScoreKeeper();
-        assert.equal();
+    it('should check if over is a valid score' , function(){
+        let instance = CricketScoreKeeper();
+        instance.setOver('21224332433413')
+        instance.setOver('--1-w-')
+
+        assert.equal(instance.isValidScore(false));
+        assert.equal(instance.isValidScore(true))
+    })
+
+    it('should get the number of overs for the game' , function(){
+        let instance = CricketScoreKeeper();
+        instance.setOver(5)
+        instance.setOver(2)
+
+        assert.equal(instance.setOver(30));
+        assert.equal(instance.setOver(12));
+    })
+
+    it('should return the total score for each over', function(){
+        let instance = CricketScoreKeeper();
+        instance.scoreKeeper('------')
+        instance.scoreKeeper('-w-2-3')
+        instance.scoreKeeper('-6-11-3')
+        instance.scoreKeeper('-10-2-w')
+        instance.scoreKeeper('12--2-3')
+
+        assert.equal(instance.overScoreTotal(0));
+        assert.equal(instance.overScoreTotal(5));
+        assert.equal(instance.overScoreTotal(20));
+        assert.equal(instance.overScoreTotal(12));
+        assert.equal(instance.overScoreTotal(17));
+    })
+
+    // it('should return the overall total for the game' , function(){
+    //     let instance = CricketScoreKeeper();
+    //     instance.overScoreTotal(5)
+    //     instance.overScoreTotal(10)
+    //     instance.overScoreTotal(6)
+
+    //     assert.equal(instance.overallTotalScore(21));
+    // })
+
+    it('should give the number of wickets left', function(){
+        let instance = CricketScoreKeeper();
+        instance.scoreKeeper('-w-2-3')
+        instance.scoreKeeper('ww--3-')
+
+        assert.equal(instance.wicketsLeft(9));
+        assert.equal(instance.wicketsLeft(8));
     })
 
 });
